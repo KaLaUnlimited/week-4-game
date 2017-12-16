fff9a633d9dc0a073ae63daa3063253032327ff4
KaLaUnlimited <KLasode@gmail.com>
2017-12-16 17:15:06 -0500

    notation added to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 
-	 var win =0;
+	var win =0;
 	var loss=0;
 	var currentNumber =0;
 	var targetNumber; 
 
-
+//start button including initializing current number and the target number and displaying on DOM
     $('#beginBtn').click(function(){
     	win=0;
     	loss=0;
@@ -36,17 +36,17 @@
 
   });
 
-	
+	//game class including all buttons
     
   	 $(".game").click(function() {
- 
+ 	//concatenating current number variables with each click and displaying updated values to DOM
  		 currentNumber +=(parseInt($(this).val()));
 		
 		
   		$("#currentNumber").text(currentNumber);
-  		 
+  		 //current number and target number for scoring
   		 if(currentNumber>targetNumber){
-			
+			debugger;
 			loss++;
 			$("#lossNumber").text(loss);
 			
@@ -59,7 +59,7 @@
 			currentNumber=0;
 			$("#currentNumber").empty();
 		}
-
+		/// if current number is equal to target number win score gets incremented by one and value is displayed on DOM
 
 		if(currentNumber===targetNumber){
 		
@@ -70,6 +70,7 @@
 		
 			targetNumber=(Math.floor(Math.random()*101) +19);
 			$("#targetNumber").text(targetNumber);
+			// reset current number to 0 and empty DOM element
 			currentNumber=0;
 			$("#currentNumber").empty();
 }
